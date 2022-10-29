8649f4c23b318c01a3f0b8b49f32f65b4f1014f4
oasj <oasjatoba1@gmail.com>
2022-10-29 06:37:56 -0300

    Implementação da câmera que segue o jogador

--- prototipo/fase.py
+++ prototipo/fase.py
@@ -10,7 +10,7 @@ class Fase:
         self.__configuracoes = Configuracoes()
 
         #Cria dois grupos de sprites, as que estão visíveis e as que são obstáculos
-        self.visible_sprites = pg.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pg.sprite.Group()
 
         #Cria o mapa baseado em um arquivo csv
@@ -39,5 +39,26 @@ class Fase:
 
     def run(self, screen):
         #Desenha as sprites visíveis e atualiza elas
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.__jogador)
         self.visible_sprites.update()
+
+
+class YSortCameraGroup(pg.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.__display_surface = pg.display.get_surface()
+        self.__half_width = self.__display_surface.get_size()[0] // 2
+        self.__half_height = self.__display_surface.get_size()[1] // 2
+        self.__offset = pg.math.Vector2()
+
+    def custom_draw(self, player):
+        # Offset é o deslocamento que a posição dos sprites visíveis deve ter para simular uma câmera
+        self.__offset.x = player.rect.centerx - self.__half_width
+        self.__offset.y = player.rect.centery - self.__half_height
+
+        # Ao invés de desenharmos os objetos numa posição fixa, nos os posicionamos de acordo com a posição do jogador.
+        # Essa posição sempre segue uma disntância fixa do jogador.
+        # Além disso, a ordem de desenho dos objetos na tela é crescente com relação a sua posição y
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.__offset
+            self.__display_surface.blit(sprite.image, offset_pos)
