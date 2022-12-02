ff3350e4300fdf37a61ae64b731a11114d6f0a92
Geovane Schmitz <contato@geovanems.com.br>
2022-12-02 11:26:24 -0300

    Reduz a quantidade de colisores para um melhor desempenho

--- versao_final/gerenciador_de_grupos.py
+++ versao_final/gerenciador_de_grupos.py
@@ -68,6 +68,56 @@ class GerenciadorDeGrupos():
 
         self.__blocos = blocos
 
+    def __unir_tiles_proximas(self, tiles: List[Tile]) -> List[Tile]:
+        novas_tiles_por_y = {}
+        for tile in tiles:
+            if tile.rect.y not in novas_tiles_por_y:
+                novas_tiles_por_y[tile.rect.y] = [tile]
+            else:
+                novas_tiles_por_y[tile.rect.y].append(tile)
+
+        for novas_tiles in novas_tiles_por_y.values():
+            novas_tiles.sort(key=lambda tile: tile.rect.x)
+            i = 0
+            while i < len(novas_tiles) - 1:
+                if novas_tiles[i].rect.x + novas_tiles[i].rect.width == novas_tiles[i + 1].rect.x and \
+                        novas_tiles[i].rect.height == novas_tiles[i + 1].rect.height:
+                    x = novas_tiles[i].rect.x
+                    y = novas_tiles[i].rect.y
+                    largura = novas_tiles[i].rect.width + novas_tiles[i + 1].rect.width
+                    altura = novas_tiles[i].rect.height
+                    superficie = novas_tiles[i].superficie
+                    tile = Tile((x, y), superficie=superficie,  largura=largura, altura=altura)
+                    novas_tiles[i] = tile
+                    del novas_tiles[i + 1]
+                else:
+                    i += 1
+        novas_tiles_por_x = {}
+        for novas_tiles in novas_tiles_por_y.values():
+            for tile in novas_tiles:
+                if tile.rect.x not in novas_tiles_por_x:
+                    novas_tiles_por_x[tile.rect.x] = [tile]
+                else:
+                    novas_tiles_por_x[tile.rect.x].append(tile)
+                    
+        for novas_tiles in novas_tiles_por_x.values():
+            novas_tiles.sort(key=lambda tile: tile.rect.y)
+            i = 0
+            while i < len(novas_tiles) - 1:
+                if novas_tiles[i].rect.y + novas_tiles[i].rect.height == novas_tiles[i + 1].rect.y and \
+                        novas_tiles[i].rect.width == novas_tiles[i + 1].rect.width:
+                    x = novas_tiles[i].rect.x
+                    y = novas_tiles[i].rect.y
+                    largura = novas_tiles[i].rect.width
+                    altura = novas_tiles[i].rect.height + novas_tiles[i + 1].rect.height
+                    superficie = novas_tiles[i].superficie
+                    tile = Tile((x, y), superficie=superficie,  largura=largura, altura=altura)
+                    novas_tiles[i] = tile
+                    del novas_tiles[i + 1]
+                else:
+                    i += 1
+        return [tile for novas_tiles in novas_tiles_por_x.values() for tile in novas_tiles]
+
     def __gerar_colisores(self, dados: pytmx.TiledMap) -> None:
 
         colisores = []
@@ -78,7 +128,7 @@ class GerenciadorDeGrupos():
                         posicao = (x * self.__configuracao.tamanho_tile, y * self.__configuracao.tamanho_tile)
                         colisores.append(Tile(posicao=posicao, superficie=superficie))
 
-        self.__colisores = colisores
+        self.__colisores = self.__unir_tiles_proximas(colisores)
 
     def __gerar_estruturas(self, dados: pytmx.TiledMap) -> None:
 
@@ -122,3 +172,4 @@ class GerenciadorDeGrupos():
 
     def matar_entidade(self, entidade: Entidade) -> None:
         self.__entidades.remove(entidade)
+
